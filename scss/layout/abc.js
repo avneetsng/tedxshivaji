f216c8f9643b96245edb1582242d914a0b82ac8b
Avneet <avneet.singh80@gmail.com>
2019-09-12 13:04:28 +0530

    fixes

---
+++ scss/layout/abc.js
@@ -0,0 +1,32 @@
+//  var a ={
+//      'a':1,
+//      'b':2,
+//      'c':3
+//  }
+
+//   var interchange =(obj)=>{
+
+          
+//       }
+
+//   }
+var list1=[1,2,3];
+var list2=[4,5,6];
+
+function add(list1,list2){
+    var list3;
+
+    for(i=0; i<list1.length ; i++)
+    {
+         list3[i] = list1[i]+list2[i];
+    }
+
+
+    return list3
+}
+
+add(list1,list2);
+
+
+
+  
